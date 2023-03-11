3018cc0e6b7bf253bdab60fcfb291c732b5e3e68
MyEngineeringLife <124250994+MyEngineeringLife@users.noreply.github.com>
2023-03-11 14:18:40 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import {Route,Routes} from 'react-router-dom'
+import Navbar2 from './components/Navbar2';
+import Community from './pages/Community';
+import Profile from './pages/Profile';
 function App() {
   return (
     <div className="App">
-      <p className='font-bold text-3xl'>Bye</p>
+      <Navbar2 />
+      
+      <Routes>
+      <Route path='/community' element={<Community/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      
+
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/Community.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const Community = () => {
+  return (
+    <div>
+      {/* <Navbar2 /> */}
+      <div class="grid grid-cols-9">
+
+        <div class="col-span-2">
+            02
+            <h2>hiiiiiiii</h2>
+        </div>
+        <div class="col-span-5">
+            main
+            <h2>heloooooooo</h2>
+        </div>
+        <div class="col-span-2">
+            02
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Community
