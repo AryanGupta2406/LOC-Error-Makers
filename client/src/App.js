e2cb54a9a236ba937e6c79f184c23adc473a64af
MyEngineeringLife <124250994+MyEngineeringLife@users.noreply.github.com>
2023-03-11 12:44:07 +0530

    try2

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className='font-bold text-3xl'>Hi</p>
+      <p className='font-bold text-3xl'>Bye</p>
     </div>
   );
 }
