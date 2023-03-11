3c29f322c8ddcbe0990ec9d481424d4e9b8564bd
MyEngineeringLife <124250994+MyEngineeringLife@users.noreply.github.com>
2023-03-11 15:33:08 +0530

    basic layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
-c
 import ImageCarousel from '../components/ImageCarousel'
 import LiveWave from '../components/LiveWave'
-import Navbar2 from '../components/Navbar2'
 
 const Home=()=> {
-  const navigate = useNavigate()
   return (
     <div>
-        <Navbar2/>
+        {/* <Navbar2/> */}
 <section class="text-gray-700 body-font bg-[#008080]">
   <div class="container mx-auto flex px-5 lg:py-8 lg:px-4 md:flex-row flex-col items-center">
     <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center lg:ml-4">
@@ -17,12 +14,8 @@ const Home=()=> {
       </h1>
       <p class="mb-8 leading-relaxed font-medium font-dm text-black">A fast and accurate API that detects human faces in images, while filtering out cartoon or distorted images. It provides probabilities of the presence of a face and can even detect the gender of the person in the image.</p>
       <div class="flex justify-center">
-        <button class="inline-flex text-black bg-white py-2 px-6 focus:outline-none rounded text-lg font-dm border-black border-2 font-medium hover:bg-[#ff7f50]" onClick={() => {
-                  navigate(`/TryApi`)
-                }}>Try FacePilot</button>
-        <button class="border-black border-2 font-medium ml-4 inline-flex text-black font-dm bg-[#008080] py-2 px-6 focus:outline-none rounded text-lg hover:bg-[#ff7f50]" onClick={() => {
-                  navigate(`/doc`)
-                }}>Documentation</button>
+        <button class="inline-flex text-black bg-white py-2 px-6 focus:outline-none rounded text-lg font-dm border-black border-2 font-medium hover:bg-[#ff7f50]">Try FacePilot</button>
+        <button class="border-black border-2 font-medium ml-4 inline-flex text-black font-dm bg-[#008080] py-2 px-6 focus:outline-none rounded text-lg hover:bg-[#ff7f50]">Documentation</button>
       </div>
     </div>
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 lg:mr-8">
