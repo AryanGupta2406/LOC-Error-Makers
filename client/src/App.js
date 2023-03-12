29ea18ce2800cb26284cbeb6bfc6bde3f6ec3d0c
merul01 <merulpshah@gmail.com>
2023-03-12 10:28:21 +0530

    Aryan se le raha

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
       <Route path='/community' element={<Community/>}/>
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/courses' element={<Courses/>}/>
-      <Route path='/home' element={<Home/>}/>
+      <Route path=' ' element={<Home/>}/>
       <Route path='/blog' element={<Blog/>}/>
       <Route path='/profilemain' element={<ProfileMain/>}/>
       <Route path='/gallery' element={<Gallery/>}/>
